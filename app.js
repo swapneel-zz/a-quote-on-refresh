13ff0b75fbc238f568124bfd6337e3146cd06dc9
Amit Bidwai <abidwai@splunk.com>
2019-06-17 13:52:00 +0530

    Quote Application

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname+'/index.html');
+});
+
+
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function(request, response) {
+  console.log("Server is up and running on http://localhost:"+port);
+});
